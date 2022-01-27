c95b0e5f6fc7477758fcecebb934f7f24a85d1fa
xiaolin <462441355@qq.com>
2022-01-27 23:00:27 +0800

    * 引入饿了么plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 2022,
+    host: '0.0.0.0',
+    proxy: {}
+  },
   plugins: [vue()]
 })
