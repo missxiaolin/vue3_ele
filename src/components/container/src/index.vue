b34f9b8e8d12c2a5873429ec7ce28cdda12b1635
xiaolin <462441355@qq.com>
2022-02-03 21:19:40 +0800

    * 侧边栏

--- src/components/container/src/index.vue
+++ src/components/container/src/index.vue
@@ -1,6 +1,6 @@
 <template>
   <el-container>
-    <el-aside width="200px">
+    <el-aside width="auto">
       <el-menu
         :collapse="isCollapse"
         default-active="2"
@@ -11,17 +11,22 @@
           <span>导航1</span>
         </el-menu-item>
         <el-menu-item index="2">
-           <el-icon-menu />
+          <el-icon-menu />
           <span>导航2</span>
         </el-menu-item>
         <el-menu-item index="3">
-           <el-icon-menu />
+          <el-icon-menu />
           <span>导航3</span>
         </el-menu-item>
       </el-menu>
     </el-aside>
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <span @click="toggle">
+          <el-icon-expand v-if="isCollapse"></el-icon-expand>
+          <el-icon-fold v-else></el-icon-fold>
+        </span>
+      </el-header>
       <el-main>
         <router-view></router-view>
       </el-main>
@@ -31,7 +36,17 @@
 
 
 <script lang="ts" setup>
+import { ref } from "vue";
+
+let isCollapse = ref(false);
+
+let toggle = () => {
+  isCollapse.value = !isCollapse.value;
+};
 </script>
 
 <style lang="scss" scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+}
 </style>
