95c466f39e2be65d37a4135a909b40256bdd124d
xiaolin <462441355@qq.com>
2022-02-07 14:33:46 +0800

    * 图标循环显示

--- src/components/chooseIcon/src/index.vue
+++ src/components/chooseIcon/src/index.vue
@@ -4,12 +4,21 @@
         <slot></slot>
     </el-button>
     <el-dialog :title="title" v-model="dialogVisible">
-        1111
+        <div class="container">
+          <div class="item" v-for="(item, index) in Object.keys(ElIcons)" :key="index">
+            <div class="text">
+              <component :is='`el-icon-${toLine(item)}`'></component>
+            </div>
+            <div class="icon">{{item}}</div>
+          </div>
+        </div>
     </el-dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
+import * as ElIcons from '@element-plus/icons-vue'
+import { toLine } from '../../../utils/index'
 import { ref, watch } from 'vue'
 let props = defineProps<{
   // 弹出框标题
@@ -36,4 +45,31 @@ watch(() => dialogVisible.value, val => {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+.item {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  font-size: 12px;
+  height: 70px;
+}
+.text {
+  font-size: 12px;
+}
+.icon {
+  flex: 1;
+}
+svg {
+  width: 2em;
+  height: 2em;
+}
 </style>
