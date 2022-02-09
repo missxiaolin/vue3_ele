9a42fea03597ee6b164754a21c6cc64ada2e0997
xiaolin <462441355@qq.com>
2022-02-09 12:42:36 +0800

    * icon 组件

--- src/components/chooseIcon/src/index.vue
+++ src/components/chooseIcon/src/index.vue
@@ -5,7 +5,7 @@
     </el-button>
      <el-dialog :title="title" v-model="dialogVisible">
         <div class="container">
-          <div class="item" v-for="(item, index) in Object.keys(ElIcons)" :key="index">
+          <div class="item" @click="clickItem(item)" v-for="(item, index) in Object.keys(ElIcons)" :key="index">
             <div class="text">
               <component :is='`el-icon-${toLine(item)}`'></component>
             </div>
@@ -20,6 +20,7 @@
 <script lang="ts" setup>
 import * as ElIcons from '@element-plus/icons-vue'
 import { toLine } from '../../../utils/index'
+import { useCopy } from '../../../hooks/useCopy/index'
 import { ref, watch } from 'vue'
 let props = defineProps<{
   // 弹出框标题
@@ -35,6 +36,12 @@ let dialogVisible = ref<boolean>(props.visible)
 let handleClick = () => {
   emits('update:visible', !props.visible)
 };
+
+let clickItem = (item: string) => {
+  let text: string = `<el-icon-${toLine(item)} />`
+  useCopy(text)
+  dialogVisible.value = false
+}
 
 // 监听数据变化
 watch(() => props.visible, val => {
