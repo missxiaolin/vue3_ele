72b816e3007efbf162a609c033b19f0794f6e296
xiaolin <462441355@qq.com>
2022-02-09 21:18:58 +0800

    * 导入

--- src/components/chooseArea/src/index.vue
+++ src/components/chooseArea/src/index.vue
@@ -1,10 +1,24 @@
 <template>
     <div>
-        省市区选择器
+        <el-select placeholder="请选择省份" v-model="province">
+            <el-option value="1">1</el-option>
+        </el-select>
+         <el-select style="margin: 0 10px;" placeholder="请选择城市" v-model="city">
+            <el-option value="1">2</el-option>
+        </el-select>
+         <el-select placeholder="请选择区域" v-model="area">
+            <el-option value="1">3</el-option>
+        </el-select>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+import allArea from '../lib/pc-code.json'
+
+let province = ref<string>('')
+let city = ref<string>('')
+let area = ref<string>('')
 
 </script>
 
