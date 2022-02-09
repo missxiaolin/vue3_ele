fef190a21226274e61d2cbaff307c5f98c682368
xiaolin <462441355@qq.com>
2022-02-09 20:42:22 +0800

    * 初始化省市区组件

---
+++ src/views/chooseArea/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <chooseArea />
+</template>
+
+<script lang="ts" setup>
+import chooseArea from '../../components/chooseArea/src/index.vue'
+
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
