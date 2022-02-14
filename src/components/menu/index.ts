ede8b2775c2dd123bb586f3b59bab584305e4480
xiaolin <462441355@qq.com>
2022-02-14 17:44:33 +0800

    * 左边菜单栏

--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -1,6 +1,6 @@
 import { App } from 'vue'
 import menu from './src/index.vue'
-import infiniteMenu from './src/menu.vue'
+import infiniteMenu from './src/menu'
 
 // 让这个组件可以通过use的形式使用
 export default {
