ef4f8b876d46e02c18ebaeb22b21d3ec73ee1499
xiaolin <462441355@qq.com>
2022-02-15 13:56:21 +0800

    * 菜单栏

---
+++ src/views/menu/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div style="display: flex">
+    <div style="flex: 1">
+      <l-menu
+        :data="data1"
+        defaultActive="1"
+        name="a"
+        index="b"
+        icon="c"
+        children="d"
+      ></l-menu>
+    </div>
+  </div>
+</template>
+
+<script lang='ts' setup>
+let data1 = [
+  {
+    a: "导航1",
+    b: "1",
+    c: "Document",
+  },
+  {
+    a: "导航2",
+    b: "2",
+    c: "Document",
+  },
+  {
+    a: "导航3",
+    b: "3",
+    c: "Document",
+    d: [
+      {
+        a: "导航3-1",
+        b: "3-1",
+        c: "Document",
+      },
+    ],
+  },
+];
+</script>
+
+<style lang='scss' scoped>
+</style>
