67b9ec3b5e590f167c5bd2bcb30679fe9b06e114
xiaolin <462441355@qq.com>
2022-02-15 13:27:06 +0800

    * 增加组件

--- src/views/notification/index.vue
+++ src/views/notification/index.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        notification
-    </div>
+  <div>
+    <l-notification :value="50">
+      <template #default>
+        <l-list @clickItem="clickItem" @clickAction="clickAction" :list="list" :actions="actions"></l-list>
+      </template>
+    </l-notification>
+  </div>
 </template>
 
-<script lang="ts" setup>
+<script lang='ts' setup>
+import { list, actions } from './data'
 
+let clickItem = (val: any) => {
+  console.log(val)
+}
+let clickAction = (val: any) => {
+  console.log(val)
+}
 </script>
 
-<style lang="scss" scoped>
-    
+<style lang='scss' scoped>
 </style>
