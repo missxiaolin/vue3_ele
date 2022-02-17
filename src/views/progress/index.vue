6888d2b111b0f9663a60b54d24f2ac4df95bbc13
xiaolin <462441355@qq.com>
2022-02-17 20:54:28 +0800

    * 组件

--- src/views/progress/index.vue
+++ src/views/progress/index.vue
@@ -1,11 +1,15 @@
 <template>
-    <l-progress></l-progress>
+  <div>
+    <l-progress isAnimate :percentage="60"></l-progress>
+    <br />
+    <l-progress isAnimate status="success" :stroke-width="20" :percentage="60"></l-progress>
+    <br />
+    <l-progress :time="5000" type="circle" isAnimate :percentage="60"></l-progress>
+  </div>
 </template>
 
-<script lang="ts" setup>
-
+<script lang='ts' setup>
 </script>
 
-<style lang="scss" scoped>
-    
+<style lang='scss' scoped>
 </style>
