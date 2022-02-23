815ac63a39bcb33cc76a7be0fcaa574060a5c82c
xiaolin <462441355@qq.com>
2022-02-23 14:56:31 +0800

    * 定义好rule

--- src/components/form/src/index.vue
+++ src/components/form/src/index.vue
@@ -1,10 +1,28 @@
 <template>
     <div>
-        form
+        <el-form ref="form" v-if="model">
+            <template v-for="(item, index) in options" :key="index">
+                <el-form-item v-if="!item.children || !item.children!.length">
+                    
+                </el-form-item>
+            </template>
+        </el-form>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { PropType, ref, onMounted, watch, nextTick } from 'vue'
+import { FormOptions } from './types/types'
+
+let model = ref<any>(null)
+
+let props = defineProps({
+    // 表单的配置项
+  options: {
+    type: Array as PropType<FormOptions[]>,
+    required: true
+  },
+})
 
 </script>
 
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-        <l-form></l-form>
-    </div>
+  <div>
+    <l-form :options="options"></l-form>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { FormOptions } from "../../components/form/src/types/types";
 
+let options: FormOptions[] = [
+  {
+    type: "input",
+    value: "",
+    label: "用户名",
+    prop: "username",
+    placeholder: "请输入用户名",
+    rules: [
+      {
+        required: true,
+        message: "用户名不能为空",
+        trigger: "blur",
+      },
+      {
+        min: 2,
+        max: 6,
+        message: "用户名在2-6位之间",
+        trigger: "blur",
+      },
+    ],
+    attrs: {
+      clearable: true,
+    },
+  },
+];
 </script>
 
 <style lang="scss" scoped>
-    
 </style>
