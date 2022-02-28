26080d269d7b440a1055b895ce2b290dbb3e47a2
xiaolin <462441355@qq.com>
2022-02-28 14:11:28 +0800

    * mock

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 2022,
+    port: 8080,
     host: '0.0.0.0',
     proxy: {}
   },
