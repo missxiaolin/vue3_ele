216241623b7077745afe596c18736127535ee43b
xiaolin <462441355@qq.com>
2022-03-04 17:35:56 +0800

    * 打包准备

---
+++ src/script/build.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const fsExtra = require('fs-extra')
+const fs = require('fs')
+const { defineConfig, build } = require('vite')
+const vue = require('@vitejs/plugin-vue')
+const vueJsx = require('@vitejs/plugin-vue-jsx')
+
+/**
+ * 全量构建
+ */
+const buildAll = async () => {
+
+}
+
+/**
+ * 打包
+ */
+const buildLib = async () => {
+    await buildAll()
+}
+
+
+buildLib()
