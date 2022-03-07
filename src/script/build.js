626e877e5bc50f54cae3c14cd2228c4e1a131853
xiaolin <462441355@qq.com>
2022-03-07 11:29:23 +0800

    * 打包文件测试

--- src/script/build.js
+++ src/script/build.js
@@ -6,13 +6,41 @@ const vue = require('@vitejs/plugin-vue')
 const vueJsx = require('@vitejs/plugin-vue-jsx')
 
 const entryDir = path.resolve(__dirname, '../components')
-const outputDir = path.resolve(__dirname, '../../vue3_ele')
+const outputDir = path.resolve(__dirname, '../../l-ui')
+
+const baseConfig = defineConfig({
+    configFile: false,
+    publicDir: false,
+    plugins: [vue(), vueJsx()]
+})
+
+
+const rollupOptions = {
+    external: ['vue', 'vue-router'],
+    output: {
+        globals: {
+            vue: 'Vue'
+        }
+    }
+}
 
 /**
  * 全量构建
  */
 const buildAll = async () => {
-
+    await build({
+        ...baseConfig,
+        build: {
+            rollupOptions,
+            lib: {
+                entry: path.resolve(entryDir, 'index.ts'),
+                name: 'index',
+                fileName: 'index',
+                formats: ['es', 'umd']
+            },
+            outDir: outputDir
+        }
+    })
 }
 
 /**
