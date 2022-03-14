88d3d48b28f5e7cd6e1f1f61505f92613d36d9f3
xiaolin <462441355@qq.com>
2022-03-14 14:23:20 +0800

    * add

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import * as Icons from '@element-plus/icons-vue'
-import './mock'
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as Icons from "@element-plus/icons-vue";
+import "./mock";
 
-import lUI from './components'
+import lUI from "./components";
 
-import router from './router/index'
-import { toLine } from './utils'
+import router from "./router/index";
+import { toLine } from "./utils";
 
-import './assets/styles/overall.scss'
-import './assets/styles/color.scss'
-import './assets/styles/resetElement.scss'
+import "./assets/styles/overall.scss";
+import "./assets/styles/color.scss";
+import "./assets/styles/resetElement.scss";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router).use(ElementPlus).use(lUI)
+app.config.warnHandler = function () {
+  // `trace` 是组件的继承关系追踪
+};
+
+app.use(router).use(ElementPlus).use(lUI);
 
 // 全局注册图标
 for (let i in Icons) {
-    app.component(`el-icon-${toLine(i)}`, (Icons as any)[i])
+  app.component(`el-icon-${toLine(i)}`, (Icons as any)[i]);
 }
 
-app.mount('#app')
+app.mount("#app");
