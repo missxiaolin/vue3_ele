b88ee9313f19e1ebbc5e058cd4e8d82cde311f41
xiaolin <462441355@qq.com>
2022-03-14 10:10:49 +0800

    * add 包

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+// 引入等比适配插件
+const px2rem = require("postcss-px2rem");
+// 配置基本大小
+const postcss = px2rem({
+  // 基准大小 baseSize，需要和rem.js中相同
+  remUnit: 14,
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 8080,
-    host: '0.0.0.0',
-    proxy: {}
+    host: "0.0.0.0",
+    proxy: {},
   },
-  plugins: [vue(), vueJsx()]
-})
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       prependData: `
+  //         @import "@/assets/styles/color.scss";
+  //       `,
+  //     },
+  //     postcss: {
+  //       plugins: [postcss],
+  //     },
+  //   },
+  // },
+  plugins: [vue(), vueJsx()],
+});
