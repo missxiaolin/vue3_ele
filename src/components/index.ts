e103b808db168703cb25888028ec55578a649ec4
xiaolin <462441355@qq.com>
2022-03-20 20:18:02 +0800

    * 图标也打包进去

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,47 +1,53 @@
-import { App } from 'vue'
+import { App } from "vue";
 
-import chooseArea from './chooseArea'
-import chooseIcon from './chooseIcon'
-import trend from './trend'
-import menu from './menu'
-import container from './container'
-import chooseTime from './chooseTime'
-import chooseDate from './chooseDate'
-import notification from './notification'
-import list from './list'
-import chooseCity from './chooseCity'
-import progress from './progress'
-import calendar from './calendar'
-import form from './form'
-import modalForm from './modalForm'
-import table from './table'
+import chooseArea from "./chooseArea";
+import chooseIcon from "./chooseIcon";
+import trend from "./trend";
+import menu from "./menu";
+import container from "./container";
+import chooseTime from "./chooseTime";
+import chooseDate from "./chooseDate";
+import notification from "./notification";
+import list from "./list";
+import chooseCity from "./chooseCity";
+import progress from "./progress";
+import calendar from "./calendar";
+import form from "./form";
+import modalForm from "./modalForm";
+import table from "./table";
 
-import '../styles/base.scss'
-import '../styles/ui.scss'
+import "../styles/base.scss";
+import "../styles/ui.scss";
+import * as Icons from "@element-plus/icons-vue";
+import { toLine } from "../utils/index";
 
 const components: any = [
-    chooseArea,
-    chooseIcon,
-    trend,
-    menu,
-    container,
-    chooseTime,
-    chooseDate,
-    notification,
-    list,
-    chooseCity,
-    progress,
-    calendar,
-    form,
-    table,
-    modalForm
-]
+  chooseArea,
+  chooseIcon,
+  trend,
+  menu,
+  container,
+  chooseTime,
+  chooseDate,
+  notification,
+  list,
+  chooseCity,
+  progress,
+  calendar,
+  form,
+  table,
+  modalForm,
+];
 
 export default {
-    // 让这个组件可以通过use的形势使用
-    install(app: App) {
-        components.map((item: any) => {
-            app.use(item)
-        })
-    }
-}
+  // 让这个组件可以通过use的形势使用
+  install(app: App) {
+    components.map((item: any) => {
+      // 全局注册图标
+      for (let i in Icons) {
+        app.component(`el-icon-${toLine(i)}`, (Icons as any)[i]);
+      }
+      app.use(item);
+    });
+  },
+};
