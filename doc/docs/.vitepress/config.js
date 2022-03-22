8f41ca08deb0a3c5bfc04499de78917c120291e6
xiaolin <462441355@qq.com>
2022-03-22 20:28:43 +0800

    * nav 配置

---
+++ doc/docs/.vitepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // 网站标题
+    title: '基于element-plus二次封装组件',
+    // 部署的基础路径
+    base: '/',
+    // 配置网站的header 标签
+    head: [
+
+    ],
+    // 主体配色
+    themeConfig: {
+        // 头部导航
+        nav: [
+            {
+                // 导航名称
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: 'github',
+                link: 'https://github.com/missxiaolin/vue3_ele',
+                target: '_blank'
+            }
+        ]
+    }
+}
