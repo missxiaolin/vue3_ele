79273de6945bdd2168a4ebff438486f71f7cf610
xiaolin <462441355@qq.com>
2022-03-24 17:54:50 +0800

    * 展示哪里有问题

--- doc/docs/.vitepress/theme/index.js
+++ doc/docs/.vitepress/theme/index.js
@@ -3,6 +3,7 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 // import lUI from "../../../../src/components";
 import lUI from '../../../../l-ui/index.es'
+import '../../../../l-ui/style.css'
 
 export default {
   ...DefaultTheme,
--- doc/docs/chooseCity/index.md
+++ doc/docs/chooseCity/index.md
@@ -1,1 +1,20 @@
 ## 省市区选择
+
+### 基本用法
+<br>
+
+<div style="padding: 1em; margin: 1em; border: 1px solid #eee;">
+    <l-choose-city></l-choose-city>
+</div>
+
+### 代码示例
+<script>
+
+</script>
+
+```js
+<l-choose-city></l-choose-city>
+<script>
+
+</script>
+```
